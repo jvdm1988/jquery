2c4f2e701a59a2fffec86be8ce075f0b36c24227
Jessicavandermarel <vandermareljessica@gmail.com>
2017-06-09 11:56:10 -0400

    done

--- app.js
+++ app.js
@@ -29,3 +29,32 @@ $(document).ready(function () {
     $('#dino-img').css('left', leftAmount + 'px');
   });
 });
+
+var  currentImageState = "normal";
+
+$("#src-btn").click(function() {
+  if (currentImageState === "normal") {
+    $("src-image").prop("src", "https://s-media-cache-ak0.pinimg.com/originals/39/09/fb/3909fb71670c490a100c3e1d792af935.jpg");
+    $currentImageState = "parody";
+  }
+
+  else {
+  $("src-image").prop("src", "http://webneel.com/daily/sites/default/files/images/daily/06-2013/8-starbucks-coffee-star-wars-coffee-logo-parody.jpg");
+  currentImageState = "normal";
+}
+
+  $("#inout-btn").click(function (){
+    var pastedImage = $("#image-input").val();
+    $("#src-image").prop("src", pastedImage);
+  });
+});
+
+
+$(document).ready(function (){
+  var currentCount = 0;
+
+  $("#increase-btn").click(function() {
+    currentCount += 1;
+    $("#count-tag").html(currentCount);
+  });
+});
